0bd8aa802c428ac8d5a5c01b91552e2cb88540ff
Aditya Soni <adityasoni5859@gmail.com>
2017-03-31 23:46:59 +0530

    Initial commit with SplashActivity

---
+++ app/src/main/java/com/example/adi/news_app/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.adi.news_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private int SPLASH_TIMER=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        //Initiating Splash Intent CountDown
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIMER);
+    }
+}
