f643e516f863ea9e5684aae790f19b5928596f25
Aditya Soni <adityasoni5859@gmail.com>
2017-04-01 08:30:33 +0530

    Applying Toolbar,toolbar Theme and modify animations

--- app/src/main/java/com/example/adi/news_app/MainActivity.java
+++ app/src/main/java/com/example/adi/news_app/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.adi.news_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //loading Activity Transition animation
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        //Initializing Variables
+        toolbar=(Toolbar)findViewById(R.id.main_toolbar);
+        //Setting Up the toolbar
+        setSupportActionBar(toolbar);
     }
 }
