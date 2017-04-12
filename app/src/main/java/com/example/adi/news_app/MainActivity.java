9bddded08c507c15884b842b6beaa0ab5d678d79
Aditya Soni <adityasoni5859@gmail.com>
2017-04-12 15:58:51 +0530

    Set up forgot password actions and signup

--- app/src/main/java/com/example/adi/news_app/MainActivity.java
+++ app/src/main/java/com/example/adi/news_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.adi.news_app;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Toolbar toolbar;
     private static final String url_one="https://adityasoni104.000webhostapp.com/news_api_one.php";
     private Button login;
-    private TextView sign_up,forgot_pass;
+    private Button sign_up,forgot_pass;
     private TextInputEditText user_email,user_password;
     private TextInputLayout email_wrapper,password_wrapper;
     private String u_email="",u_password="NONE";
-
+    ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,8 +48,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getSupportActionBar().setTitle(null);
         //Initializing the components
         login=(Button)findViewById(R.id.login_submit);
-        sign_up=(TextView)findViewById(R.id.sign_up);
-        forgot_pass=(TextView)findViewById(R.id.forgot_pass);
+        sign_up=(Button) findViewById(R.id.sign_up);
+        forgot_pass=(Button)findViewById(R.id.forgot_pass);
         user_email=(TextInputEditText) findViewById(email);
         user_password=(TextInputEditText) findViewById(R.id.password);
         email_wrapper=(TextInputLayout)findViewById(R.id.email_wrapper);
@@ -58,10 +58,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         login.setOnClickListener(this);
         sign_up.setOnClickListener(this);
         forgot_pass.setOnClickListener(this);
-
+        ///Initializing progress bar
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
     }
     private void makeCustomHttpCalls(final String url,final String email,final String password) {
-        final String url1=url;
+        pDialog.show();
         //making Http Calls and Requests
         StringRequest stringRequest=new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
             @Override
@@ -103,8 +105,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if(success.equals("1")){
                 Intent i=new Intent(this,HomeActivity.class);
                 startActivity(i);
+                finish();
             }
             else if(success.equals("0")){
+                pDialog.dismiss();
                 Snackbar snackbar = Snackbar
                         .make(findViewById(R.id.view_pallet), "Eighter Email or Password is incorrect", Snackbar.LENGTH_LONG);
                 snackbar.show();
@@ -123,18 +127,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id;
         id=view.getId();
         switch (id){
-            case R.id.sign_up:
-                Intent i1=new Intent(this,SignUpActivity.class);
-                startActivity(i1);
-            case R.id.forgot_pass:
-                Intent i2=new Intent(this,ForgotPasswordActivity.class);
-                startActivity(i2);
             case R.id.login_submit:
                 ///Getting Content
                 u_email=email_wrapper.getEditText().getText().toString();
                 u_password=password_wrapper.getEditText().getText().toString();
                 ///Making Http Call
                 makeCustomHttpCalls(url_one,u_email,u_password);
+                break;
+            case R.id.sign_up:
+                Intent i1=new Intent(this,SignUpActivity.class);
+                startActivity(i1);
+                finish();
+                break;
+            case R.id.forgot_pass:
+                Intent i2=new Intent(this,ForgotPasswordActivity.class);
+                startActivity(i2);
+                finish();
+                break;
         }
     }
 }
--- app/src/main/java/com/example/adi/news_app/SignUpActivity.java
+++ app/src/main/java/com/example/adi/news_app/SignUpActivity.java
@@ -1,11 +1,37 @@
 package com.example.adi.news_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.adi.news_app.supporting_dir.CustomVolleyApp;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String url_two="https://adityasoni104.000webhostapp.com/news_api_two.php";
     private Toolbar toolbar;
+    private TextInputEditText email;
+    private TextInputLayout email_wrapper;
+    private Button send_password;
+    private String u_email="";
+    ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +43,74 @@ public class SignUpActivity extends AppCompatActivity {
         //Setting Up the toolbar
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
+        //Initializing Components
+        email_wrapper=(TextInputLayout)findViewById(R.id.email_wrapper);
+        send_password=(Button)findViewById(R.id.submit);
+        ///Setting up onclick listener
+        send_password.setOnClickListener(this);
+        ///Initializing progress bar
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+    }
+    private void makeCustomHttpCalls(final String url,final String email) {
+        pDialog.show();
+        //making Http Calls and Requests
+        StringRequest stringRequest=new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //System.out.println(response.toString());
+                setResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Something Went Wrong !");
+                error.printStackTrace();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parameters=new HashMap<String, String>();
+                parameters.put("email",email);
+                return parameters;
+            }
+        };
+        //Adding to Request Queue
+        CustomVolleyApp.getInstance().addToRequestQueue(stringRequest);
+    }
+
+    private void setResponse(String response) {
+        JSONObject jsonObejct=null;
+        try {
+            jsonObejct=new JSONObject(response);
+        } catch (JSONException e) {
+            Snackbar snackbar = Snackbar
+                    .make(findViewById(R.id.view_pallet), "Internet Connection Error,try Again Later", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            e.printStackTrace();
+        }
+        try {
+            String success=jsonObejct.getString("success");
+            if(success.equals("1")){
+                Intent i=new Intent(this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+            else{
+                pDialog.dismiss();
+                Snackbar snackbar = Snackbar
+                        .make(findViewById(R.id.view_pallet), "Oops,Enter correct Details and try Again !", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        u_email=email_wrapper.getEditText().getText().toString();
+        //Toast.makeText(getApplicationContext(),"Hello "+u_email,Toast.LENGTH_LONG).show();
+        makeCustomHttpCalls(url_two,u_email);
     }
 }
